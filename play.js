3797888a1caa5f88357a78d2a6c74116e2224f70
gisellealmario <gisellealmario@gmail.com>
2023-10-31 06:25:22 +0000

    snake with client and play

--- play.js
+++ play.js
@@ -1,27 +1,5 @@
-const net = require("net");
+const connect = require('./client')
 
-// Establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
-
-  // Interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // Event handler for errors
-  conn.on("error", (error) => {
-    console.error("Connection error:", error);
-  });
-
-  // Event handler for when the connection is closed
-  conn.on("close", () => {
-    console.log("Connection closed.");
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
-const connection = connect();
+connect();
