795c85499f5823c714ab99e8e3e49e83916c51ba
gisellealmario <gisellealmario@gmail.com>
2023-10-31 19:41:16 +0000

    conn.on

--- client.js
+++ client.js
@@ -5,6 +5,19 @@ const connect = function () {
     host: "165.227.47.243",
     port: 50541,
   });
+  
+  conn.on("connected", () => {
+    console.log("Successfully connected to game server" );
+    setName(conn, "GTR");
+
+  });
+  conn.on("error", (error) => {
+    console.error("Connection error:", error);
+  });
+  conn.on("close", () => {
+    console.log("Connection closed.");
+  });
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 const connect = require('./client')
 
+// establishes a connection with the game server
 
 console.log("Connecting ...");
 connect();
