4f704b305f42a0f1162074ca72d89caa8b205cff
gisellealmario <gisellealmario@gmail.com>
2023-11-01 06:04:26 +0000

    handleuserinput

--- client.js
+++ client.js
@@ -1,16 +1,23 @@
 const net = require("net");
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: "165.227.47.243",
+    host: "165.227.47.243 ",
     port: 50541,
   });
-  
-  conn.on("connected", () => {
-    console.log("Successfully connected to game server" );
-    setName(conn, "GTR");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game server");
+    conn.write("Name: GTR");
+    conn.write("Move: up"); // Send "Move: up" when you connect
   });
+
+  conn.setEncoding("utf8");
+
+  setInterval(() => {
+    conn.write("Move: up");
+  }, 50);
+
   conn.on("error", (error) => {
     console.error("Connection error:", error);
   });
@@ -20,7 +27,7 @@ const connect = function () {
 
 
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+
 
   return conn;
 };
